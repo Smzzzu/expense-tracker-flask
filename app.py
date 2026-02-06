4f5d8ce47afe589051d6556875b5dbc3c0793e21
Smzzzu <sammer80521@gmail.com>
2026-02-06 16:41:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,5 +24,8 @@ def add():
 
     return redirect("/")
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
